99c8859749f935b0abab7b5022f5abbeba4ebac8
dahee-heo <hdahee.9@gmail.com>
2024-07-29 23:37:40 +0900

    MODIFY: [회원] body record  api 수정 및 추가

--- src/services/member/body.ts
+++ src/services/member/body.ts
@@ -1,29 +1,34 @@
 import { api } from "@/utils/axios"
 
 export const getBody = async (params: string) => {
-  return await api.get(`/api/v1/members/body?dateTime=${params}`)
+  return await api.get(`/api/v1/members/body-info?uploadDate=${params}`)
 }
 
-export const getBodyImage = async (params: string) => {
-  return await api.get(`/api/v1/members/body/image?dateTime=${params}`)
+export const getBodyImage = async (params: string, data: {}) => {
+  return await api.get(`/api/v1/members/body-info/image?uploadDate=${params}`, data)
 }
 
 export const getBodyImages = async () => {
-  return await api.get('/api/v1/members/body/images')
+  return await api.get('/api/v1/members/body-info/images')
 }
 
 export const postBody = async (data: {}) => {
-  return await api.post('/api/v1/members/body', data)
+  return await api.post('/api/v1/members/body-info', data)
 }
 
 export const postWeight = async (data: {}) => {
-  return await api.post('/api/v1/members/body/weight', data)
+  return await api.post('/api/v1/members/body-info/weight', data)
 }
 
-export const postBodyImage = async (data: {}) => {
-  return await api.post(`/api/v1/members/body/image`, data, {
+export const postBodyImage = async (data: FormData) => {
+  return await api.post(`/api/v1/members/body-info/image`, data, {
     headers: {
-      "Content-Type": 'multipart/form-data'
+      'Content-Type': 'multipart/form-data'
     }
   })
+}
+
+
+export const deleteBodyImage = async (data: {}) => {
+  return await api.delete(`/api/v1/members/body-info/image`, data)
 }
