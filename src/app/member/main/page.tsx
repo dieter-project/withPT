0ad54e8fcd037be3bc3715bb9938bd4fc308be9f
dahee-heo <hdahee.9@gmail.com>
2024-10-13 22:52:06 +0900

    MODIFY: [회원] main 페이지 EmptyData 컴포넌트 적용

--- src/app/member/main/page.tsx
+++ src/app/member/main/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import Header from '@/components/Header';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
-import { ExclamationMark, LabelTitle } from '@/styles/Text';
-import { ContentSection } from '@/styles/Layout';
-import MemberBottomNav from '@/components/MemberBottomNav';
-import { TrainerSwipe } from '@/components/TrainerSwipe';
+import Header from "@/components/Header";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { ExclamationMark, LabelTitle } from "@/styles/Text";
+import { ContentSection } from "@/styles/Layout";
+import MemberBottomNav from "@/components/MemberBottomNav";
+import { TrainerSwipe } from "@/components/TrainerSwipe";
 import {
   EmptyTodayDiet,
   GoalContents,
@@ -16,20 +16,25 @@ import {
   TodayDiet,
   TodayDietContents,
   TodayDietList,
-  TodayTab
-} from './styles';
-import DonutChart from '@/components/member/main/DonutChart';
-import WorkoutList from '@/components/member/WorkoutList';
-import { format } from 'date-fns';
-import { ko } from 'date-fns/locale';
-import { getExerciseByDate } from '@/services/member/exercise';
-import { getLessonsDays, getLessonsMonthly, getPersonalTrainers } from '@/services/member/training';
-import { getMemberInfo } from '@/services/member/member';
-import { getDietByDate } from '@/services/member/diet';
-import { MemberInfo } from '@/types/member/member';
-import { convertGoal } from '@/utils/convertGoal';
-import { ScheduleDates } from '@/types/member/schedule';
-import MonthlyCalendar from '@/components/member/MonthlyCalendar';
+  TodayTab,
+} from "./styles";
+import DonutChart from "@/components/member/main/DonutChart";
+import WorkoutList from "@/components/member/WorkoutList";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+import { getExerciseByDate } from "@/services/member/exercise";
+import {
+  getLessonsDays,
+  getLessonsMonthly,
+  getPersonalTrainers,
+} from "@/services/member/training";
+import { getMemberInfo } from "@/services/member/member";
+import { getDietByDate } from "@/services/member/diet";
+import { MemberInfo } from "@/types/member/member";
+import { convertGoal } from "@/utils/convertGoal";
+import { ScheduleDates } from "@/types/member/schedule";
+import MonthlyCalendar from "@/components/member/MonthlyCalendar";
+import EmptyData from "@/components/member/EmptyData";
 
 const page = () => {
   const router = useRouter();
@@ -50,83 +55,93 @@ const page = () => {
     targetWeight: 0,
     role: "",
     joinDate: "",
-    lastModifiedDate: ""
-  }
+    lastModifiedDate: "",
+  };
 
-  const [tabClick, setTabClick] = useState('1')
-  const [todayDiet, setTodayDiet] = useState("")
-  const [todayWorkout, setTodayWorkout] = useState([])
+  const [tabClick, setTabClick] = useState("1");
+  const [todayDiet, setTodayDiet] = useState("");
+  const [todayWorkout, setTodayWorkout] = useState([]);
   const [markDate, setMarkDate] = useState([]);
   const [activeDate, setActiveDate] = useState<ScheduleDates>(today);
   const [trainers, setTrainers] = useState([]);
-  const [memberInfo, setMemberInfo] = useState<MemberInfo>(memberInit)
+  const [memberInfo, setMemberInfo] = useState<MemberInfo>(memberInit);
 
   const handleGetTodayDiet = async () => {
     try {
-      const { data } = await getDietByDate(format(today, 'yyyy-MM-dd'))
-      setTodayDiet(data.data)
+      const { data } = await getDietByDate(format(today, "yyyy-MM-dd"));
+      setTodayDiet(data.data);
     } catch (error) {
-      console.log('error: ', error);
+      console.log("error: ", error);
     }
-  }
+  };
 
   const handleGetTodayWorkout = async () => {
     try {
-      const { data } = await getExerciseByDate(format(today, 'yyyy-MM-dd'))
-      setTodayWorkout(data.data)
+      const { data } = await getExerciseByDate(format(today, "yyyy-MM-dd"));
+      setTodayWorkout(data.data);
     } catch (error) {
-      console.log('error: ', error);
+      console.log("error: ", error);
     }
-  }
+  };
 
   const getMember = async () => {
     try {
-      const { data: { data } } = await getMemberInfo();
-      setMemberInfo(data)
+      const {
+        data: { data },
+      } = await getMemberInfo();
+      setMemberInfo(data);
     } catch (error) {
-      console.log('error: ', error);
+      console.log("error: ", error);
     }
-  }
+  };
 
   const handleGetTrainers = async (id: number) => {
     try {
-      const response = await getPersonalTrainers(id)
-      const { data: { data } } = response
-      setTrainers(data)
+      const response = await getPersonalTrainers(id);
+      const {
+        data: { data },
+      } = response;
+      setTrainers(data);
     } catch (error) {
       // console.log('error: ', error);
     }
-  }
+  };
 
   const getLesson = async () => {
     try {
-      const params = { year: format(today, "yyyy"), month: format(today, "MM") }
-      const { data: { data } } = await getLessonsMonthly(params);
+      const params = {
+        year: format(today, "yyyy"),
+        month: format(today, "MM"),
+      };
+      const {
+        data: { data },
+      } = await getLessonsMonthly(params);
     } catch (error) {
       // console.log('error: ', error);
     }
-  }
-
+  };
 
   const onChange = (value: ScheduleDates) => {
-    setActiveDate(value)
-    router.push(`/member/schedule?date=${format(new Date(value as Date), "yyyy-MM-dd")}`)
-  }
+    setActiveDate(value);
+    router.push(
+      `/member/schedule?date=${format(new Date(value as Date), "yyyy-MM-dd")}`,
+    );
+  };
 
   useEffect(() => {
     getMember();
     handleGetTodayDiet();
     handleGetTodayWorkout();
     getLesson();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    handleGetTrainers(memberInfo.id)
-  }, [memberInfo])
+    handleGetTrainers(memberInfo.id);
+  }, [memberInfo]);
 
   return (
     <>
-      <Header page='home' />
+      <Header page="home" />
       <MainWrap>
         <ContentSection>
           <MyGoal>
@@ -138,64 +153,77 @@ const page = () => {
               </div>
               <div>
                 <span></span>
-                <div>{convertGoal("exercise", memberInfo.exerciseFrequency)}</div>
+                <div>
+                  {convertGoal("exercise", memberInfo.exerciseFrequency)}
+                </div>
               </div>
             </GoalContents>
           </MyGoal>
         </ContentSection>
         <ContentSection>
-          <LabelTitle>{format(new Date(), 'MM.dd EEEE', { locale: ko })}</LabelTitle>
-          <div className='section-contents'>
+          <LabelTitle>
+            {format(new Date(), "MM.dd EEEE", { locale: ko })}
+          </LabelTitle>
+          <div className="section-contents">
             <TodayTab>
               <div
-                className={tabClick === '1' ? 'active' : ''}
-                onClick={() => setTabClick('1')}
-              >오늘의 식단</div>
+                className={tabClick === "1" ? "active" : ""}
+                onClick={() => setTabClick("1")}
+              >
+                오늘의 식단
+              </div>
               <div
-                className={tabClick === '2' ? 'active' : ''}
-                onClick={() => setTabClick('2')}
-              >오늘의 운동</div>
+                className={tabClick === "2" ? "active" : ""}
+                onClick={() => setTabClick("2")}
+              >
+                오늘의 운동
+              </div>
             </TodayTab>
             <div>
-              {tabClick === '1'
-                ? <>
-                  {todayDiet?.length > 0 ?
-                    (
-                      <TodayDiet>
-                        <TodayDietContents>
-                          <div>
-                            <div className='title'>섭취칼로리</div>
-                            <div><span>1018Kcal</span> / 1550 Kcal</div>
-                            <TodayDietList>
-                              <li>탄수화물 <strong>50%</strong></li>
-                              <li>단백질 <strong>50%</strong></li>
-                              <li>지방 <strong>50%</strong></li>
-                            </TodayDietList>
-                          </div>
-                          <div>
-                            <DonutChart />
-                          </div>
-                        </TodayDietContents>
-                      </TodayDiet>
-                    ) : (
-                      <EmptyTodayDiet>
-                        <ExclamationMark>!</ExclamationMark>
-                        <div>아직 등록된 식단이 없어요.</div>
+              {tabClick === "1" ? (
+                <>
+                  {todayDiet?.length > 0 ? (
+                    <TodayDiet>
+                      <TodayDietContents>
                         <div>
-                          눌러서 오늘의 식단을 입력해 주세요
+                          <div className="title">섭취칼로리</div>
+                          <div>
+                            <span>1018Kcal</span> / 1550 Kcal
+                          </div>
+                          <TodayDietList>
+                            <li>
+                              탄수화물 <strong>50%</strong>
+                            </li>
+                            <li>
+                              단백질 <strong>50%</strong>
+                            </li>
+                            <li>
+                              지방 <strong>50%</strong>
+                            </li>
+                          </TodayDietList>
                         </div>
-                      </EmptyTodayDiet>
-                    )
-                  }
+                        <div>
+                          <DonutChart />
+                        </div>
+                      </TodayDietContents>
+                    </TodayDiet>
+                  ) : (
+                    <EmptyData
+                      text="아직 등록된 식단이 없어요."
+                      subText="눌러서 오늘의 식단을 입력해 주세요"
+                      onClick={() => router.push('/member/record/diet/register')}
+                    />
+                  )}
                 </>
-                : <WorkoutList workout={todayWorkout} />
-              }
+              ) : (
+                <WorkoutList workout={todayWorkout} />
+              )}
             </div>
           </div>
         </ContentSection>
         <ContentSection>
           <LabelTitle>수업일정</LabelTitle>
-          <div className='section-contents'>
+          <div className="section-contents">
             <MonthlyCalendar
               activeDate={activeDate}
               setActiveDate={setActiveDate}
@@ -204,15 +232,16 @@ const page = () => {
             />
           </div>
         </ContentSection>
-        {trainers.length > 0 &&
+        {trainers.length > 0 && (
           <ContentSection>
             <LabelTitle>담당 트레이너</LabelTitle>
             <TrainerSwipe data={trainers} />
-          </ContentSection>}
+          </ContentSection>
+        )}
         <MemberBottomNav />
       </MainWrap>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/components/member/WorkoutList.tsx
+++ src/components/member/WorkoutList.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { AddRecordButton } from '@/styles/AddButton'
-import { styled } from 'styled-components'
-import { EmptyTodayDiet } from '@/app/member/main/styles'
-import { ExclamationMark } from '@/styles/Text'
-import { WorkoutPayload, WorkoutType } from '@/types/member/record'
-import { EXERCISE_TYPE } from '@/constants/record'
+import React from "react";
+import { AddRecordButton } from "@/styles/AddButton";
+import { styled } from "styled-components";
+import { EmptyTodayDiet } from "@/app/member/main/styles";
+import { ExclamationMark } from "@/styles/Text";
+import { WorkoutPayload, WorkoutType } from "@/types/member/record";
+import { EXERCISE_TYPE } from "@/constants/record";
+import EmptyData from "./EmptyData";
+import { useRouter } from "next/navigation";
 
 const WorkoutListBox = styled.div`
   li {
@@ -14,7 +16,7 @@ const WorkoutListBox = styled.div`
     &:not(:last-child) {
       border-bottom: 1px solid var(--border-gray300);
     }
-    >div {
+    > div {
       &:first-child {
         width: 3.25rem;
         height: 3.25rem;
@@ -39,57 +41,67 @@ const WorkoutListBox = styled.div`
       }
     }
   }
-`
+`;
 
 interface Props {
-  workout: WorkoutType[] | WorkoutPayload[]
+  workout: WorkoutType[] | WorkoutPayload[];
 }
 
 const WorkoutList = ({ workout }: Props) => {
-  console.log('workout: ', workout);
-  return (<>
-    {workout?.length > 0 ?
-      <WorkoutListBox>
-        <div>
-          <ul>
-            {
-              workout?.map((workout, index) => {
+  const router = useRouter();
+
+  return (
+    <>
+      {workout?.length > 0 ? (
+        <WorkoutListBox>
+          <div>
+            <ul>
+              {workout?.map((workout, index) => {
                 return (
                   <li key={index}>
                     <div>
-                      <img src={EXERCISE_TYPE.find(type => type.value === workout.exerciseType)?.src} alt="" />
+                      <img
+                        src={
+                          EXERCISE_TYPE.find(
+                            type => type.value === workout.exerciseType,
+                          )?.src
+                        }
+                        alt=""
+                      />
                     </div>
                     <div>
                       <div>{workout.title}</div>
-                      <div className='workout-contents'>
-                        {workout.exerciseType === 'ANAEROBIC' 
-                          ? `${workout.weight}kg x ${workout.exerciseTime}회 x ${workout.exerciseSet}set` 
-                          : `${EXERCISE_TYPE.find(type => type.value === workout.exerciseType)!.title}, ${workout.times}`}
+                      <div className="workout-contents">
+                        {workout.exerciseType === "ANAEROBIC"
+                          ? `${workout.weight}kg x ${workout.exerciseTime}회 x ${workout.exerciseSet}set`
+                          : `${
+                              EXERCISE_TYPE.find(
+                                type => type.value === workout.exerciseType,
+                              )!.title
+                            }, ${workout.times}`}
                       </div>
                     </div>
                   </li>
-                )
-              })
-            }
-          </ul>
-          {/* <AddRecordButton
+                );
+              })}
+            </ul>
+            {/* <AddRecordButton
           onClick={handleAddWorkout}
           style={{
             height: "48px",
           }}
         /> */}
-        </div>
-      </WorkoutListBox>
-      : <EmptyTodayDiet>
-        <ExclamationMark>!</ExclamationMark>
-        <div>아직 등록된 운동이 없어요.</div>
-        <div>
-          눌러서 오늘의 운동을 입력해 주세요
-        </div>
-      </EmptyTodayDiet>
-    }
-  </>
-  )
-}
+          </div>
+        </WorkoutListBox>
+      ) : (
+        <EmptyData
+          text="아직 등록된 운동이 없어요."
+          subText="눌러서 오늘의 운동을 입력해 주세요"
+          onClick={() => router.push("/member/record/workout/register")}
+        />
+      )}
+    </>
+  );
+};
 
-export default WorkoutList
+export default WorkoutList;
