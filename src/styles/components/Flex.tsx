0873a19962147e6cc206d5a468b13302649fc6a8
dahee-heo <hdahee.9@gmail.com>
2024-10-27 20:55:41 +0900

    FEAT: [회원] flex box 컴포넌트 생성

---
+++ src/styles/components/Flex.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const FlexRowStart = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`
+
+export const FlexRowCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const FlexRowEnd = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`
+
+export const FlexRowBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const FlexColumnStart = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+`
+
+export const FlexColumnCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const FlexColumnEnd = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: center;
+`
+
+export const FlexColumnBetween = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
