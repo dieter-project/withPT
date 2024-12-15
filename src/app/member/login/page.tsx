fab3707dea641ac24eabc29858ac79f125a577bc
dahee-heo <hdahee.9@gmail.com>
2024-12-15 22:06:53 +0900

    MODIFY: [회원] 이메일 로그인 후 페이지 이동

--- src/app/member/login/page.tsx
+++ src/app/member/login/page.tsx
@@ -1,26 +1,34 @@
 'use client';
 
+import React from 'react'
 import PageHeader from '@/components/PageHeader';
 import { BaseContentWrap } from '@/styles/Layout';
-import { getCsrfToken, signIn, useSession } from 'next-auth/react';
-import Link from 'next/link';
-import React, { useEffect } from 'react'
-import { styled } from 'styled-components'
+import { signIn } from 'next-auth/react';
 import { GoogleLoginButton, KakaoLoginButton, LoginButtonWrap, LogoWrap } from './style';
 import Image from 'next/image';
+import { api } from '@/utils/axios';
+import { useRouter } from 'next/navigation';
 
 
 const page = () => {
-
-  useEffect(() => {
-
-  }, [])
+  const route = useRouter()
 
   const onKakaoSocialLogin = (): any => {
     const redirectUri = 'http://localhost:3000/callback/kakao'
     const restApiKey = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${restApiKey}&redirect_uri=${redirectUri}&response_type=code`
     window.location.href = kakaoURL
+  }
+
+  const handleEmailLogin = async () => {
+    try {
+      const response = await api.post('/api/v1/auth/login', {
+        email: 'member@test.com',
+        password: 'member1234',
+        role: 'MEMBER'
+      })
+      if (response.status === 200) route.replace('/member/main')
+    } catch (err) {}
   }
 
   return (
@@ -44,6 +52,7 @@ const page = () => {
             onClick={() => signIn('google')}>
             Google로 시작하기
           </GoogleLoginButton>
+          <button style={{fontSize: '14px'}} onClick={handleEmailLogin}>email 로그인</button>
         </LoginButtonWrap>
       </BaseContentWrap>
     </>
